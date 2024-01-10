55be0c19442a65759443b99a8820df3a3b6d0d0a
Isaac <Nagasis3321@gmail.com>
2024-01-10 10:14:49 -0300

    Advances in system views prior to the start of the backEnd

--- EmsaProyectFrontEnd/src/app/Component/new-sector/new-sector.component.ts
+++ EmsaProyectFrontEnd/src/app/Component/new-sector/new-sector.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { CreateSectorComponent } from './create-sector/create-sector.component';
+import { DeleteSectorComponent } from './delete-sector/delete-sector.component';
+import { ModifySectorComponent } from './modify-sector/modify-sector.component';
+import { FindSectorComponent } from './find-sector/find-sector.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-sector',
   standalone: true,
-  imports: [],
+  imports: [CreateSectorComponent,DeleteSectorComponent,ModifySectorComponent,FindSectorComponent,CommonModule,FormsModule],
   templateUrl: './new-sector.component.html',
   styleUrl: './new-sector.component.scss'
 })
 export class NewSectorComponent {
+  optionCompontent:"New"| "Modify" |"Find" |"Delete" = "Find";
 
+  cargaComponent(option:"New"| "Modify" |"Find" |"Delete" ):void {
+    this.optionCompontent=option;
+  }
 }

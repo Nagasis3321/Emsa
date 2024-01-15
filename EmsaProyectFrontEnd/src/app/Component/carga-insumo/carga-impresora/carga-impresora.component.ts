00afd71f9c634187b9bc73ba2b874be31c180e4a
Isaac <Nagasis3321@gmail.com>
2024-01-15 11:54:12 -0300

    prior developer BackEnd, view new Computer and printer

--- EmsaProyectFrontEnd/src/app/Component/carga-insumo/carga-impresora/carga-impresora.component.ts
+++ EmsaProyectFrontEnd/src/app/Component/carga-insumo/carga-impresora/carga-impresora.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CommonEngine } from '@angular/ssr';
 
 @Component({
   selector: 'app-carga-impresora',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './carga-impresora.component.html',
-  styleUrl: './carga-impresora.component.scss'
+  styleUrl: './carga-impresora.component.scss',
 })
 export class CargaImpresoraComponent {
+  solicitante: string = '';
+  gerencia: string = '';
+  telefono: string = '';
+  nombreEquipo: string = '';
+  idOrdenReclamo: number = 0;
+  detalleIngreso: string = '';
+  sectores: { id: number; name: string; nomenclatura: string }[] = [
+    { id: 1, name: 'San Vicente', nomenclatura: 'SV' },
+    { id: 2, name: 'Jardin America', nomenclatura: 'JA' },
+    { id: 3, name: 'Posadas', nomenclatura: 'PO' },
+    { id: 4, name: 'Campo Grande', nomenclatura: 'CG' },
+    { id: 5, name: 'Iguazu', nomenclatura: 'I' },
+  ];
+  sectorSeleccionado: { id: number; name: string; nomenclatura: string } = {
+    id: 3,
+    name: 'Posadas',
+    nomenclatura: 'PO',
+  };
 
+  cargarImpresora() {}
+
+  limpiar(): void {
+    this.solicitante = '';
+    this.gerencia = '';
+    this.telefono = '';
+    this.nombreEquipo = '';
+    this.detalleIngreso = '';
+    this.telefono = '';
+  }
 }
--- EmsaProyectFrontEnd/src/app/Component/carga-insumo/carga-insumo.component.ts
+++ EmsaProyectFrontEnd/src/app/Component/carga-insumo/carga-insumo.component.ts
@@ -7,16 +7,19 @@ import { CargaMonitorComponent } from './carga-monitor/carga-monitor.component';
 @Component({
   selector: 'app-carga-insumo',
   standalone: true,
-  imports: [CargaMonitorComponent,CargaImpresoraComponent, CargaComputadoraComponent, CommonModule],
+  imports: [
+    CargaMonitorComponent,
+    CargaImpresoraComponent,
+    CargaComputadoraComponent,
+    CommonModule,
+  ],
   templateUrl: './carga-insumo.component.html',
-  styleUrl: './carga-insumo.component.scss'
+  styleUrl: './carga-insumo.component.scss',
 })
 export class CargaInsumoComponent {
-  optionCompontent:"Impresora" | "Computadora" | "Monitor" = "Computadora";
+  optionCompontent: 'Impresora' | 'Computadora' | 'Monitor' = 'Impresora';
 
-  cargaComponent(option:"Impresora" | "Computadora" | "Monitor"):void {
-    this.optionCompontent=option;
+  cargaComponent(option: 'Impresora' | 'Computadora' | 'Monitor'): void {
+    this.optionCompontent = option;
   }
-
-
 }
